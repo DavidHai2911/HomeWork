64d0c205c23d2f4ddf66698cf6fb63d59dcbbcd7
DavidHai <bestgaming2911@gmail.com>
2025-09-14 19:26:59 +0700

    OOP Book

---
+++ Code3/TextBook.java
@@ -0,0 +1,30 @@
+package Code;
+
+public class TextBook extends Book {
+
+    private String subject;
+
+    public TextBook(String id, String title, double basePrice, String subject) {
+        super(id, title, basePrice);
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Subject: " + subject + ", Price: " + calculatePrice());
+    }
+
+    @Override
+    public double calculatePrice() {
+        return getBasePrice() * 1.1;
+    }
+}
